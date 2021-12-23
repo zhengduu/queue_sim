cfc542becc45fd093030982736844889367b9802
Zheng Du <duzheng96@yahoo.de>
2021-12-23 11:50:58 +0100

    Update files

--- queue_sim/queue_plots.py
+++ queue_sim/queue_plots.py
@@ -12,68 +12,71 @@ import os
 import sys
 
 
-def plot_dispersion(input_folder1, input_folder2, input_folder3, input_file, 
+def plot_dispersion(input_folder1, input_folder2, input_folder3, input_file,
                     plot, save):
-    
+
     serving_bitrate = 1e9
     n_queues = [input_folder1.strip(" - ")[1], input_folder2.strip(" - ")[1],
-                input_folder3.strip(" - ")[1],]
+                input_folder3.strip(" - ")[1], ]
     sys_load = [20, 50, 90]
-    
+
     # file_folder = r'C:\Zheng Data\TU Delft\Thesis\Thesis Work\GitHub\queue_sim\Output\test'
-    file_folder = os.getcwd().strip("queue_sim") + "Output"    
+    file_folder = os.getcwd().strip("queue_sim") + "Output\\local"
     # "trace_APP50_end.csv"
-    file_to_plot1 = file_folder + '\\' + input_folder1 + '\\' + input_file 
+    file_to_plot1 = file_folder + '\\' + input_folder1 + '\\' + input_file
     file_to_plot2 = file_folder + '\\' + input_folder2 + '\\' + input_file
     file_to_plot3 = file_folder + '\\' + input_folder3 + '\\' + input_file
-    
-    output_folder = os.getcwd().strip("queue_sim") + '\\Plots\\'  
-    
+
+    output_folder = os.getcwd().strip("queue_sim") + '\\Plots\\'
+
     data1 = pd.read_csv(file_to_plot1, encoding='utf-8')
     data2 = pd.read_csv(file_to_plot2, encoding='utf-8')
     data3 = pd.read_csv(file_to_plot3, encoding='utf-8')
-    
-    np_data = data1.to_numpy()  
+
+    np_data = data1.to_numpy()
     plot_data1 = np_data.flatten()
-    
-    np_data = data2.to_numpy()  
+
+    np_data = data2.to_numpy()
     plot_data2 = np_data.flatten()
 
-    np_data = data3.to_numpy()  
+    np_data = data3.to_numpy()
     plot_data3 = np_data.flatten()
 
-    
     tti_duration = 0.00025
     fps = 30
     key_int = 10
     GoP_duration = key_int * (1/30)
-    total_duration = np.ceil(plot_data1[-1])
-    total_ttis = int((total_duration / tti_duration) / 10)
+    total_duration = np.ceil(plot_data3[-1])
+    total_ttis = 1000 # int((total_duration / tti_duration) / 2)
     GoP_ttis = int(GoP_duration / tti_duration)
-    
+
     packets_per_tti1 = np.zeros((total_ttis))
     packets_per_tti2 = np.zeros((total_ttis))
     packets_per_tti3 = np.zeros((total_ttis))
-    
+
     # Create list with how many packets arrive per TTI
-    for tti in range(total_ttis): 
+    for tti in range(total_ttis):
         # if tti == total_ttis - 1: print(tti)
         start_time = tti * tti_duration
         end_time = start_time + tti_duration
-        
-        packet_list1 = np.where((plot_data1 >= start_time) & (plot_data1 < end_time))        
+
+        packet_list1 = np.where((plot_data1 >= start_time)
+                                & (plot_data1 < end_time))
         packets_per_tti1[tti] = np.size(packet_list1)
-        
-        packet_list2 = np.where((plot_data2 >= start_time) & (plot_data2 < end_time))        
+
+        packet_list2 = np.where((plot_data2 >= start_time)
+                                & (plot_data2 < end_time))
         packets_per_tti2[tti] = np.size(packet_list2)
-        
-        packet_list3 = np.where((plot_data3 >= start_time) & (plot_data3 < end_time))        
+
+        packet_list3 = np.where((plot_data3 >= start_time)
+                                & (plot_data3 < end_time))
         packets_per_tti3[tti] = np.size(packet_list3)
+
         # if np.size(packet_list) == 0:
-        #     packets_per_tti[tti] = float('nan')           
+        #     packets_per_tti[tti] = float('nan')
         # else:
-        #    packets_per_tti[tti] = np.size(packet_list)          
-        
+        #    packets_per_tti[tti] = np.size(packet_list)
+
     # print("TTIs: ", np.size(np.where(packets_per_tti > 0)))
 
     """
@@ -127,66 +130,69 @@ def plot_dispersion(input_folder1, input_folder2, input_folder3, input_file,
     sd_load_10_P = [2.165, 0.395, 0.077]
     sd_load_15_P = [2.165, 0.395, 0.077]
     """
-    
+
     # raise SystemExit
-    x_axis = np.arange(0,int(total_ttis))
-    x_axis_GoP = np.arange(0,GoP_ttis)
-    
+    x_axis = np.arange(0, int(total_ttis))
+    x_axis_GoP = np.arange(0, GoP_ttis)
+
     x_axis_queues = ['5', '10', '15']
     x_axis_load = ['20%', '50%', '90%']
-    
+
     # plot = False
     if plot:
-#############################
-#############################        
-        plt.figure(figsize=(30,20))
+        #############################
+        #############################
+        plt.figure(figsize=(30, 20))
         plt.title("Arrival packets per TTI after all queues" + "\n" +
-                  "VR background traffic", 
-                  # f"{input_folder1.split('- VR')[0]} & \n" + 
-                  # f"{input_folder2.split('- VR')[0]} & \n" + 
+                  "Random background traffic" + 
+                  # f"{input_folder1.split('- VR')[0]} & \n" +
+                  # f"{input_folder2.split('- VR')[0]} & \n" +
                   # f"{input_folder3.split('- VR')[0]}",
-                    # f"\n Serving Bitrate: {serving_bitrate/1000000}Mbps" +  
-                    # "\n Load: 20%",
-                    # f"\n Number of queues: {n_queues[0]}",  
-                     # f"\n System Load: {sys_load[0]}%",                     
-                     fontsize=20, fontweight='bold') 
-        
+                  # f"\n Serving Bitrate: {serving_bitrate/1000000}Mbps" +
+                  "\n Load: 90%",
+                  # f"\n Number of queues: {n_queues[0]}",
+                  # f"\n System Load: {sys_load[0]}%",
+                  fontsize=20, fontweight='bold')
+
         # plt.subplot(121)
         # plt.title('Mean - time spend in total by packets of each frame', fontsize=20)
         plt.xlabel('TTIs', fontsize=20)
         plt.xticks(fontsize=16)
         plt.ylabel('Number of packets', fontsize=20)
         plt.yticks(fontsize=16)
-        plt.ylim(0, np.nanmax(np.concatenate([packets_per_tti1, 
+        plt.ylim(0, np.nanmax(np.concatenate([packets_per_tti1,
                                               packets_per_tti2,
-                                              packets_per_tti3])) + 5)  
-        
-        # plt.scatter(x_axis, packets_per_tti1[0:int(total_ttis)], marker='s', 
+                                              packets_per_tti3])) + 2)
+
+        # plt.scatter(x_axis, packets_per_tti1[0:int(total_ttis)], marker='s',
         #             s=100, label='20% Load') # 0:int(total_ttis/2)
-        plt.plot(x_axis, packets_per_tti1[0:int(total_ttis)], # '-s',  
-                  label='20% Load')
-        
-        # plt.scatter(x_axis, packets_per_tti2[0:int(total_ttis)], marker='v', 
-        #             s=100, label='50% Load')
-        plt.plot(x_axis, packets_per_tti2[0:int(total_ttis)], # '-*', 
-                 label='50% Load')
-        
-        # plt.scatter(x_axis, packets_per_tti3[0:int(total_ttis)], marker='o', 
+        plt.plot(x_axis, packets_per_tti1[0:int(total_ttis)], alpha=0.7, # '-s',
+                  # label='20% Load')
+                  label='5 Queues (1.0s)')
+    
+        # # plt.scatter(x_axis, packets_per_tti2[0:int(total_ttis)], marker='v',
+        # #             s=100, label='50% Load')
+        # plt.plot(x_axis, packets_per_tti2[0:int(total_ttis)], alpha=0.7, # '-*',
+        #           # label='50% Load')
+        #           label='10 Queues (1.0s)')
+    
+        # plt.scatter(x_axis, packets_per_tti3[0:int(total_ttis)], marker='o',
         #             s=100, label='90% Load')
-        plt.plot(x_axis, packets_per_tti3[0:int(total_ttis)], # '-o', 
-                 label='90% Load')
+        # plt.plot(x_axis, packets_per_tti3[0:int(total_ttis)], alpha=0.7, # '-o',
+        #           # label='90% Load')
+        #           label='15') 
         plt.legend(prop={'size': 25})
         # plt.show()
-        print(input_folder3.split('VR - ')[1])
-        
+        # print(input_folder3.split('BG - ')[1])
+
         if(save):
             save_path = output_folder + "New" + "\\"
-            save_name = f'Packets per TTI - Queues_{input_folder3.split("VR - ")[1]}_cut2.png'
+            save_name = f'Packets per TTI - VR - Queues - Load 90_10-15.png'
             plt.savefig(save_path + save_name, dpi=300, bbox_inches='tight')
             print(f'Figure saved: {save_name}')
-                
-        return # raise SystemExit
-        
+
+        return  # raise SystemExit
+
         """
         # TODO: Make plots with multiple inputs - show multiple queues together
 #############################
@@ -237,22 +243,18 @@ def plot_dispersion(input_folder1, input_folder2, input_folder3, input_file,
                 
         # save = False
         """
-           
-    
 
 
 input_folder = "\\5Q - 20.0% Load - 1Gbps - 1.0s\\"
 input_folder2 = "\\5Q - 50.0% Load - 1Gbps - 1.0s\\"
 input_folder3 = "\\5Q - 90.0% Load - 1Gbps - 1.0s\\"
 
-input_folder1 = "5Q - 20.0% Load - 1Gbps - 1.0s - VR - 10.0us"
-input_folder2 = "5Q - 50.0% Load - 1Gbps - 1.0s - VR - 10.0us"
-input_folder3 = "5Q - 90.0% Load - 1Gbps - 1.0s - VR - 10.0us"
+input_folder1 = "5Q - 90.0% Load - 1Gbps - 1.0s - BG - 10.0us"
+input_folder2 = "10Q - 50.0% Load - 1Gbps - 1.0s - BG - 10.0us"
+input_folder3 = "15Q - 50.0% Load - 1Gbps - 1.0s - BG - 10.0us"
 
 # input_name = "\\10Q - 3.0s - 1Gbps - 20.0% Load\\"
 input_file = "trace_APP50_end.csv"
 
-plot_dispersion(input_folder1, input_folder2, input_folder3, input_file, 
-                plot = True, save = not True)    
-
-
+plot_dispersion(input_folder1, input_folder2, input_folder3, input_file,
+                plot=True, save= not True)
--- queue_sim/queue_sim_utils.py
+++ queue_sim/queue_sim_utils.py
@@ -9,7 +9,10 @@ import numpy as np
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 
+
+tic = time.perf_counter()
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -27,7 +30,7 @@ file_to_simulate = file_folder + "VR_traces" + "\\" + "trace_APP50.csv"
 # Load into dataframe
 sim_data = pd.read_csv(file_to_simulate, encoding='utf-16-LE') 
                 
-sim_time = 0.999 
+sim_time = 59.999 
 
 # Adjust timestamps to avoid sorting problems!  
 # Packet timestamp count starts at zero 
@@ -37,8 +40,8 @@ sim_time = 0.999
 fps = 30 # int(np.ceil(sim_data["frame"].iloc[-1] / sim_data["time"].iloc[-1]))
 frame_time = 1 / fps       
 
-# Cut dataframe until total simulation time by frame number
-sim_data = sim_data[((sim_data['frame']/fps) <= sim_time)] 
+# # Cut dataframe until total simulation time by frame number
+# sim_data = sim_data[((sim_data['frame']/fps) <= sim_time)] 
 
 sim_data['time'] = sim_data['frame'] * frame_time 
 
@@ -79,9 +82,9 @@ for frame in range(sim_data['frame'].iloc[-1] + 1):
 nr_packets_in_frame = list(range(len(packets_in_frame)))
 interpacket_time_frame = list(range(len(packets_in_frame)))
 
-dispersion = 0.000001
+burstiness = 0.6
 
-dispersion_per_frame = frame_time * (dispersion)
+dispersion_per_frame = frame_time * (1 - burstiness)
 
 for frame in range(len(packets_in_frame)):
     # Add one microsecond inter-packet time 
@@ -115,15 +118,19 @@ for frame in range(len(packets_in_frame)):
 # bps = sum(sizes) * 8 / total_time # bits / seconds
 
 # print(bps/1e6)
+toc = time.perf_counter()
 
+print(f"{toc-tic:0.4f}")
 plot_data = sim_data['time'].to_numpy()
 
+
+# %%
 tti_duration = 0.00025
 fps = 30
 key_int = 10
 GoP_duration = key_int * (1/30)
 total_duration = np.ceil(plot_data[-1])
-total_ttis = int((total_duration / tti_duration) / 1)
+total_ttis = 4000 # int((total_duration / tti_duration) / 240)
 GoP_ttis = int(GoP_duration / tti_duration)
 
 packets_per_tti1 = np.zeros((total_ttis))
@@ -157,9 +164,12 @@ plt.xlabel('TTIs', fontsize=20)
 plt.xticks(fontsize=16)
 plt.ylabel('Number of packets', fontsize=20)
 plt.yticks(fontsize=16)
-plt.ylim(0, np.nanmax(packets_per_tti1) + 5)  
+plt.ylim(0, np.nanmax(packets_per_tti1) + 3)  
 
 # plt.scatter(x_axis, packets_per_tti1[0:int(total_ttis)], marker='s', 
 #             s=100, label='20% Load') # 0:int(total_ttis/2)
-plt.plot(x_axis, packets_per_tti1[0:int(total_ttis)], # '-s',  
+plt.bar(x_axis, packets_per_tti1[0:int(total_ttis)], # '-s',  
           label='20% Load')
+
+
+
