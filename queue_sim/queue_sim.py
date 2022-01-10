d556c2a9934e405d0c7a4bb9e29c2e9d9d9d3747
Zheng Du <duzheng96@yahoo.de>
2022-01-10 23:41:24 +0100

    Update queue_sim.py

--- queue_sim/queue_sim.py
+++ queue_sim/queue_sim.py
@@ -134,7 +134,7 @@ def initialise_event_calendar(vr_file_name, seed, vr_timestamps, vr_sizes,
         
     # Calculate exponential inter-packet arrival time through packet 
     # distribution and desired system load 
-    bg_throughput = sys_load * 1e9 # In Gbps
+    bg_throughput = sys_load * 0.98 * 1e9 # In Gbps
     avg_packet_size = round(np.sum(packet_sizes * packet_prob)) * 8 # in bit
     nr_packets_per_s = int(bg_throughput / avg_packet_size)
     exp_time = round((1 / nr_packets_per_s), 9)
